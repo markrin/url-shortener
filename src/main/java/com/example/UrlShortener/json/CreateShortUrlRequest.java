01fac54f639099dbc681b7ada898ccaba2af7f5b
markrin <markrules@ya.ru>
2023-07-10 17:04:49 +0300

    init
    Signed-off-by: markrin <markrules@ya.ru>

---
+++ src/main/java/com/example/UrlShortener/json/CreateShortUrlRequest.java
@@ -0,0 +1,16 @@
+package com.example.UrlShortener.json;
+
+//test
+public class CreateShortUrlRequest {
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    private String longUrl;
+
+}
